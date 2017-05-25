ca4846f9edd96840adff5c38ea530211674c0473
Mike Lambert <lambert8@illinois.edu>
2017-05-25 12:05:30 -0500

    Respect enableLogs in gulp task as well...
    also added a "verbose" option to cut out the spam during gulp build

--- src/system.cachebuster.gulp.js
+++ src/system.cachebuster.gulp.js
@@ -8,6 +8,8 @@ function SystemJSCacheBuster (options) {
     options = options || {};
 
     var outputFileName = options.output || "system.cachebuster.json";
+    this.enableLogs = options.enableLogs || true;
+    this.verbose = options.verbose || false;
     this.outputFilePath = path.join(process.cwd(), outputFileName);
     this.baseDir = options.baseDir || process.cwd();
     this.hashes = {};
@@ -33,19 +35,25 @@ SystemJSCacheBuster.prototype.incremental = function() {
 }
 
 SystemJSCacheBuster.prototype._processFile = function (file) {
-    var hash = crypto.createHash('sha1').update(file._contents).digest('hex');
+    if (file._contents != null) {
+        var hash = crypto.createHash('sha1').update(file._contents).digest('hex');
 
-    var relFilePath = upath.normalize(path.relative(this.baseDir, file.path));
-    this.hashes[relFilePath] = {
-        path: relFilePath,
-        hash: hash,
-    };
+        var relFilePath = upath.normalize(path.relative(this.baseDir, file.path));
+        this.hashes[relFilePath] = {
+            path: relFilePath,
+            hash: hash,
+        };
 
-    console.log("Updating hash: " + relFilePath + " --> " + hash);
+        if (this.enableLogs && this.verbose) {
+            console.log("Updating hash: " + relFilePath + " --> " + hash);
+        }
+    }
 }
 
 SystemJSCacheBuster.prototype._flushIndex = function() {
-    console.log("Writing summary file: " + this.outputFilePath);
+    if (this.enableLogs) {
+        console.log("Writing to summary file: " + this.outputFilePath);
+    }
     
     fs.writeFileSync(this.outputFilePath, JSON.stringify(this.hashes));
 }
